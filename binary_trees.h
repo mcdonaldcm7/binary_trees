a150a2176697e04b2f5aab5df237fa4d2004b92e
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2023-06-01 11:43:01 +0100

    Added declaration for 'binary_tree_size'

--- binary_trees.h
+++ binary_trees.h
@@ -33,4 +33,5 @@ void binary_tree_inorder(const binary_tree_t *, void (*func)(int));
 void binary_tree_postorder(const binary_tree_t *, void (*func)(int));
 unsigned int binary_tree_height(const binary_tree_t *);
 unsigned int binary_tree_depth(const binary_tree_t *);
+unsigned int binary_tree_size(const binary_tree_t *);
 #endif
