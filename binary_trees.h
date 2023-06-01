924ab8160227ae7558397d23cf731b027e1e55b8
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2023-06-01 19:14:12 +0100

    Added declaration for 'binary_tree_is_full'

--- binary_trees.h
+++ binary_trees.h
@@ -37,4 +37,5 @@ unsigned int binary_tree_size(const binary_tree_t *);
 unsigned int binary_tree_leaves(const binary_tree_t *);
 unsigned int binary_tree_nodes(const binary_tree_t *);
 int binary_tree_balance(const binary_tree_t *);
+int binary_tree_is_full(const binary_tree_t *);
 #endif
