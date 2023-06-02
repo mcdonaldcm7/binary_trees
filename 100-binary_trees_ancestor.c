3e9a6da593fc19739a4f8ec516af6f2cfabe97dd
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2023-06-02 23:09:33 +0100

    Handled case: Same node, most left of right node and most right of left node

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -2,6 +2,27 @@
 #include <stdio.h>
 
 /**
+ * both_node - Traverses the both node upward to locate a common ancestor
+ *
+ * @left: Pointer to the first node (usually the left)
+ * @right: Pointer to the second node (usually the right)
+ *
+ * Return: Pointer to the lowest common ancestor node of the two given nodes
+ */
+binary_tree_t *both_node(const binary_tree_t *left,
+		const binary_tree_t *right)
+{
+	binary_tree_t *parent;
+
+	if (!left || !right)
+		return ((void *) 0);
+	parent = right->parent;
+	if (parent == left || parent == left->parent)
+		return (parent);
+	return (both_node(left->parent, parent));
+}
+
+/**
  * second_node - Traverses the second node upward to locate a common ancestor
  *
  * @left: Pointer to the first node (usually the left)
@@ -60,9 +81,20 @@ binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 	parent = (void *) 0;
 	if (!first || !second)
 		return ((void *) 0);
+	else if (first == second)
+	{
+		parent = first->parent;
+		if (parent->left == first)
+			return (parent->left);
+		else
+			return (parent->right);
+	}
 	parent = first_node(first, second);
 	if (parent)
 		return (parent);
 	parent = second_node(first, second);
+	if (parent)
+		return (parent);
+	parent = both_node(first, second);
 	return (parent);
 }
