00c7c40a131bafaee0203c89faf80ea3a018b687
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2023-06-02 18:26:36 +0100

    Defintion for 'binary_trees_ancestor'

---
+++ 100-binary_trees_ancestor.c
@@ -0,0 +1,68 @@
+#include "binary_trees.h"
+#include <stdio.h>
+
+/**
+ * second_node - Traverses the second node upward to locate a common ancestor
+ *
+ * @left: Pointer to the first node (usually the left)
+ * @right: Pointer to the second node (usually the right)
+ *
+ * Return: Pointer to the lowest common ancestor node of the two given nodes
+ */
+binary_tree_t *second_node(const binary_tree_t *left,
+		const binary_tree_t *right)
+{
+	binary_tree_t *parent;
+
+	if (!left || !right)
+		return ((void *) 0);
+	parent = right->parent;
+	if (parent == left || parent == left->parent)
+		return (parent);
+	return (second_node(left, parent));
+}
+
+/**
+ * first_node - Traverses the first node upward to locate a common ancestor
+ *
+ * @left: Pointer to the first node (usually the left)
+ * @right: Pointer to the second node (usually the right)
+ *
+ * Return: Pointer to the lowest common ancestor node of the two given nodes
+ */
+binary_tree_t *first_node(const binary_tree_t *left,
+		const binary_tree_t *right)
+{
+	binary_tree_t *parent;
+
+	if (!left || !right)
+		return ((void *) 0);
+	parent = left->parent;
+	if (parent == right || parent == right->parent)
+		return (parent);
+	return (first_node(parent, right));
+}
+
+/**
+ * binary_trees_ancestor - Find the lowest common ancestor of two nodes
+ *
+ * @first: Pointer to the first node
+ * @second: Pointer to the second node
+ *
+ * Return: Pointer to the lowest common ancestor of the two given nodes,
+ * 0 otherwise
+ */
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+		const binary_tree_t *second)
+{
+	binary_tree_t *parent;
+
+	parent = (void *) 0;
+	if (!first || !second)
+		return ((void *) 0);
+	parent = first_node(first, second);
+	if (parent)
+		return (parent);
+	parent = second_node(first, second);
+	return (parent);
+}
